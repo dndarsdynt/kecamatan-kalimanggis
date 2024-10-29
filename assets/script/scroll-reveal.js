1449e3763be2f8d906d913edc604d3b2bcea329a
dndarsdynt <a.foralpha.mail@gmail.com>
2024-10-29 10:06:41 +0700

    versi 1.0

---
+++ assets/script/scroll-reveal.js
@@ -0,0 +1,41 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    reset: true
+})
+
+
+/*SCROLL HOME*/
+sr.reveal('.scroll1', { origin: 'bottom', delay: 100})
+sr.reveal('.scroll2', { delay: 100 })
+sr.reveal('.scroll2a', { delay: 500 })
+sr.reveal('.scroll2b', { delay: 1000 })
+sr.reveal('.scroll2c', { delay: 1500 })
+sr.reveal('.scroll2d', { delay: 1700 })
+sr.reveal('.scroll2e', { delay: 1800 })
+sr.reveal('.scroll2f', { delay: 2000 })
+sr.reveal('.scroll2g', { delay: 2200 })
+
+sr.reveal('.scroll3', { origin: 'right', delay: 100 })
+sr.reveal('.scroll3a', { origin: 'right', delay: 500 })
+sr.reveal('.scroll3b', { origin: 'right', delay: 700 })
+sr.reveal('.scroll3c', { origin: 'right', delay: 900 })
+sr.reveal('.scroll3d', { origin: 'right', delay: 1100 })
+sr.reveal('.scroll3e', { origin: 'right', delay: 1300 })
+sr.reveal('.scroll3f', { origin: 'right', delay: 1500 })
+
+
+sr.reveal('.scroll4', { origin: 'left', delay: 100 })
+sr.reveal('.scroll4a', { origin: 'left', delay: 500 })
+sr.reveal('.scroll4b', { origin: 'left', delay: 700 })
+sr.reveal('.scroll4c', { origin: 'left', delay: 900 })
+sr.reveal('.scroll4d', { origin: 'left', delay: 1100 })
+sr.reveal('.scroll4e', { origin: 'left', delay: 1300 })
+sr.reveal('.scroll4f', { origin: 'left', delay: 1500 })
+
+sr.reveal('.scroll5a', { origin: 'left', delay: 1600 })
+sr.reveal('.scroll5b', { origin: 'left', delay: 1700 })
+sr.reveal('.scroll5c', { origin: 'left', delay: 1800 })
+sr.reveal('.scroll5d', { origin: 'left', delay: 1900 })
+sr.reveal('.scroll5e', { origin: 'left', delay: 2000 })
